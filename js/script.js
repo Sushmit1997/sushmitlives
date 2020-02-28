c84f566e31b2bd4e93b06d2065adfc6d455c7731
Sushmit Rajaure <39255317+Sushmit1997@users.noreply.github.com>
2020-02-28 23:47:27 +0545

    Form Update

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function ageindays(){
+    alert('Beauty lies in the eyes of beholder. This Website is Fukin-A ');
+};
+
+function callme() {
+    alert('Please Call my number as mentioned or Email me anytime. Thank you.');
+};
--- script.js
+++
@@ -1,3 +0,0 @@
-function ageindays(){
-    alert('Beauty lies in the eyes of beholder. This Website is Fukin-A ');
-};
