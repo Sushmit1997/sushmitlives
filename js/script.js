3d807614cb5c4892d1d2c813c261cb77dc651664
Sushmit Rajaure <39255317+Sushmit1997@users.noreply.github.com>
2020-02-29 22:53:34 +0545

    Theme and podcast update

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 function ageindays(){
-    alert('Beauty lies in the eyes of beholder. This Website is Fukin-A ');
+    alert('Quote for today is: "Beauty lies in the eyes of beholder."');
 };
 
 function callme() {
     alert('Please Call my number as mentioned or Email me anytime. Thank you.');
 };
+
